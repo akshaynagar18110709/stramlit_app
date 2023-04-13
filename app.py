eb4b1dcb76bac4f5769bef6d6ec05e03efc23266
akshaynagar18110709 <130655580+akshaynagar18110709@users.noreply.github.com>
2023-04-13 15:58:28 +0530

    Update app.py
    setting index to 'Fruit'

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ streamlit.header("Build your own Fruit Smoothie")
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+## set index to fruits
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 ## create a multiselect wizard.
 streamlit.multiselect("Pick your fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
