2558d894d4f9e2fb78a7e31f74803cd22a50f88a
akshaynagar18110709 <130655580+akshaynagar18110709@users.noreply.github.com>
2023-04-13 15:52:24 +0530

    Update app.py
    loaded pandas dataframe.

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ streamlit.title("My Parent new healthy diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Banana, Milk, Eggs")
 streamlit.text("Omega-3 Fatty")
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
