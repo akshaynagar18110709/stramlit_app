39653e1a71027f611a931606635186d1b761d66d
akshaynagar18110709 <130655580+akshaynagar18110709@users.noreply.github.com>
2023-04-13 15:56:30 +0530

    Update app.py
    create multiselect wizard.

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ streamlit.header("Build your own Fruit Smoothie")
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+## create a multiselect wizard.
+streamlit.multiselect("Pick your fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
